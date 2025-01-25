8802acc7fc59b05c351dc1e4bb143fcc1390933e
shatrughanshukla <shatrughanshukla00@gmail.com>
2025-01-25 16:17:00 +0530

    Ternary Operator

---
+++ L3_0TernaryOperator.java
@@ -0,0 +1,20 @@
+package JAVA;
+
+public class L3_0TernaryOperator {
+    public static void main(String[] args)
+    {
+        int a = 10;
+        int result = 0;
+        // if (a > 10)
+        // {
+        //     result = 1;
+        // }
+        // else
+        // {
+        //     result = 0;
+            
+        // }
+        result = (a > 0 ? 1 : 0); // // This si ternary operator.
+        System.out.println(result);
+    }
+}
