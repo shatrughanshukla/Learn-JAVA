15aa858b05110a96fd32568b0a1d0cc5a504dc70
shatrughanshukla <shatrughanshukla00@gmail.com>
2025-02-04 12:20:42 +0530

    Day01_commit1

---
+++ L8_0Array.java
@@ -0,0 +1,16 @@
+//package JAVA;
+
+public class L8_0Array {
+    public static void main(String[] args)
+    {
+        int num[] = new int[4];
+        num[0] = 3;
+        num[1] = 4;
+        num[2] = 5;
+        num[3] = 6;
+        for(int i = 0; i < 4; i++)
+        {
+            System.out.println(num[i]);
+        }
+    }
+}
